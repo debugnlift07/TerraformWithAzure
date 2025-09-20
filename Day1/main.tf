53673eb73c6670e3c1672486d8e2f39be0f28351
Naveen Bisht <33859786+debugbyNB@users.noreply.github.com>
2025-09-20 14:51:03 +0530

    Day1: larning terraform with azure

--- Day1/main.tf
+++ Day1/main.tf
@@ -1,14 +1,9 @@
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
 
-// careate resoure gorup
+# Create Resource Group
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "rg-learning-azure"
+  location = "SouthIndia"
 }
